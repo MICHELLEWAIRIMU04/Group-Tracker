c5833c1753b3fd4f9f12929e15149cc3b29f7e82
MICHELLEWAIRIMU04 <michelle@michelle>
2025-05-12 10:05:43 +0300

    first commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 // API service functions
 
-const API_URL = 'http://127.0.0.1:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 const getAuthHeaders = () => {
     const token = localStorage.getItem('token');
